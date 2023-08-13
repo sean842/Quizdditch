ce847ff9a9351f77a166286864bfe823879093e8
sean842 <104303480+sean842@users.noreply.github.com>
2023-08-13 19:42:52 +0300

    Add files via upload

---
+++ Shared/Models/Portelem/DataForSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleProject.Shared.Models.Portelem
+{
+    public class DataForSystem
+    {
+        public int SystemId { get; set; }
+        public string Url { get; set; }
+    }
+}
